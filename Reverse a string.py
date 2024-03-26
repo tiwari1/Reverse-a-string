8fb11b408dbe6b13e3c1dfef5ad2740414d8af7d
Akshita Tiwari <85664297+tiwari1@users.noreply.github.com>
2024-03-26 22:33:07 +0530

    Add files via upload

---
+++ Reverse a string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# WAP and take the input from the user if the len of the user is above 3 thn print hii
+# if the len of the user is below three or equal to 3 thn reverse the string
+x=input("enter the string:")
+length = len(x)
+if length >3:
+    print("hii")
+else:
+    print("the reverse string is",x[::-1])
+
+
+# In[ ]:
+
+
+
+
